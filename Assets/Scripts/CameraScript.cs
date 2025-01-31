8e2fc36c1228ef547086ad78b9d5d84ff937a6d8
Alexander Daley <alexander.daley@mymail.champlain.edu>
2025-01-31 16:04:59 -0500

    Fixed camera script

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,6 +4,7 @@ public class CameraScript : MonoBehaviour
 {
     [SerializeField] private float zLock;
     [SerializeField] private GameObject player;
+    [SerializeField] private Vector2 distanceToPlayer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +16,7 @@ public class CameraScript : MonoBehaviour
     void Update()
     {
         //updates camera position to follow the player at a constant z axis to maintain a 2-d perspective
-        gameObject.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, zLock);
+        gameObject.transform.position = new Vector3(player.transform.position.x + distanceToPlayer.x, player.transform.position.y + distanceToPlayer.y, zLock);
 
     }
 }
