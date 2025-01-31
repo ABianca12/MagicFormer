b718b72e2654b6edf43d30722a3d1de6452ef2d7
Alexander Bianca <alexander.bianca@mymail.champlain.edu>
2025-01-31 15:53:02 -0500

    Basic player movement

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float airMultiplier = 0.4f;
+    private float movementMultiplier = 10f;
+
+    [Header("Camera Effects")]
+    [SerializeField] Camera cam;
+
+    [Header("Jumping")]
+    public float jumpForce = 50f;
+    public float jumpRate = 15f;
+
+    [Header("Keybinds")]
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] KeyCode sprintKey = KeyCode.LeftShift;
+
+    [Header("Gravity and Air Control")]
+    [SerializeField] float grav;
+    [SerializeField] float groundDrag = 6f;
+    [SerializeField] float airDrag = 2f;
+
+    [Header("Ground Detection")]
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundMask;
+    public bool isGrounded { get; private set; }
+
+    [HideInInspector] public bool isMoving;
+
+    private Vector3 moveDirection;
+    private Vector2 movement;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        isMoving = (moveDirection != Vector3.zero);
+
+        HandleInput();
+        ControlDrag();
+        ControlSpeed();
+
+        if (Input.GetKey(jumpKey) && isGrounded)
+        {
+            HandleJump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void HandleInput()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        moveDirection = gameObject.transform.forward * -movement.y + gameObject.transform.right * -movement.x;
+    }
+
+    void HandleJump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void ControlSpeed()
+    {
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void ControlDrag()
+    {
+        rb.drag = isGrounded ? groundDrag : airDrag;
+    }
+
+    void MovePlayer()
+    {
+        float multiplier = isGrounded ? movementMultiplier : airMultiplier * movementMultiplier;
+        rb.AddForce(multiplier * moveSpeed * moveDirection.normalized, ForceMode.Acceleration);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint col in collision.contacts)
+        {
+            if (col.point.y <= transform.position.y + 0.1 && !isGrounded)
+            {
+                isGrounded = true;
+            }
+        }
+    }
+}
