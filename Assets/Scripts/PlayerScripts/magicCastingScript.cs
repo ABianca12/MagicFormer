c3bc4afcf521643ad58b5e7e5279b3b64badfaef
Alexander.Daley <alexander.daley@mymail.champlain.edu>
2025-02-26 17:30:27 -0500

    attempted a bugfix for force push, unsuccessful.  Locked rotation for latest 2 spells.  Will probably ask professor for his input on friday

--- Assets/Scripts/PlayerScripts/magicCastingScript.cs
+++ Assets/Scripts/PlayerScripts/magicCastingScript.cs
@@ -63,8 +63,23 @@ public class magicCastingScript : MonoBehaviour
         {
             //Debug.Log("FORCE BURST");
             ForcePush f = Instantiate(poosh);
-            Vector2 point = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            f.initForcePush(gameObject.transform.position, point.normalized * 10);
+            Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(point);
+            point.Normalize();
+            //f.initForcePush(gameObject.transform.position, new Vector2(point.x - gameObject.transform.position.x, point.y - gameObject.transform.position.y) * 10);
+            switch (p.getFaceDirection())
+            {
+                case PlayerDirection.Left:
+                    f.initForcePush(gameObject.transform.position + new Vector3(-3,0,0), new Vector2(-10,10));
+                    break;
+                case PlayerDirection.Right:
+                    f.initForcePush(gameObject.transform.position + new Vector3(3, 0, 0), new Vector2(10, 10));
+                    break;
+                default:
+                    f.initForcePush(gameObject.transform.position + new Vector3(3, 0, 0), new Vector2(10, 10));
+                    break;
+            }
+                    
         }
 
     }
