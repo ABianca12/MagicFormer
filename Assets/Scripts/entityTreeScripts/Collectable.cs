4ee46194e138595dee0b84a6a5416ce4c27bf9dd
Alexander.Daley <alexander.daley@mymail.champlain.edu>
2025-03-20 17:55:28 -0400

    laid groundwork for a player inventory and the collectables that ccome with it

--- Assets/Scripts/entityTreeScripts/Collectable.cs
+++ Assets/Scripts/entityTreeScripts/Collectable.cs
@@ -3,5 +3,31 @@ using System.Collections.Generic;
 
 public class Collectable : Interactable
 {
-    
+    //Member Variables
+    protected Inventory pInventory;
+
+    //init functions
+    public void initCollectable()
+    {
+        initInteractable();
+        pInventory = GameObject.FindWithTag("Inventory").GetComponent<Inventory>();
+    }
+    public void initCollectable(Vector3 start)
+    {
+        initInteractable(start);
+        pInventory = GameObject.FindWithTag("Inventory").GetComponent<Inventory>();
+    }
+
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        //if Player collides with the object
+        if(c.gameObject.tag == "Player")
+        {
+            //Collect Item
+
+            Destroy(gameObject);
+        }
+    }
+
 }
