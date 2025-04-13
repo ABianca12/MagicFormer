d95dca1eab59e872e0b912d9d7f142f5625e106c
Alexander.Daley <alexander.daley@mymail.champlain.edu>
2025-04-13 11:14:02 -0400

    Started framework on earth crate fixes.  Also made the UI canvas a prefab

--- Assets/Scripts/entityTreeScripts/Crate.cs
+++ Assets/Scripts/entityTreeScripts/Crate.cs
@@ -5,28 +5,35 @@ public class Crate : Pickup
 
     //member variables
     private Rigidbody2D rb;
+    [SerializeField] private BoxCollider2D collider;
 
     //Init functions
     public void initCrate()
     {
+        checkSpawn();
         base.initPickup(BaseType.STONE);
     }
     public void initCrate(BaseType m)
     {
+        checkSpawn();
         base.initPickup(m);
     }
     public void initCrate(Vector3 start)
     {
+        checkSpawn();
         base.initPickup(BaseType.STONE, start);
     }
     public void initCrate(Vector3 start, BaseType b)
     {
+        checkSpawn();
         base.initPickup(b, start);
+        
     }
 
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        collider.isTrigger = false;
     }
 
     private void FixedUpdate()
@@ -43,6 +50,19 @@ public class Crate : Pickup
         else
         {
             rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+    }
+
+    private void checkSpawn()
+    {
+        collider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == "Pickup")
+        {
+            Destroy(gameObject);
         }
     }
 }
